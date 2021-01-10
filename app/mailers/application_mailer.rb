521f6ff6ea72842105641adf243736462a267435
Alertzero <faceroxz@gmail.com>
2021-01-10 20:54:55 +0700

    Add account activation10

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreplyaratori@sandboxfa20a7ada8074e03ae87babeb4c62059.com'
+  default from: 'noreply@sandboxfa20a7ada8074e03ae87babeb4c62059.com'
   layout 'mailer'
 end

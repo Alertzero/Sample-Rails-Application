4e3989d650bd2a172cf84538c3de20b55f2d814d
Alertzero <faceroxz@gmail.com>
2021-01-10 20:04:22 +0700

    Add account activation7

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@aratori.net'
+  default from: 'noreply@sandboxfa20a7ada8074e03ae87babeb4c62059.mailgun.org'
   layout 'mailer'
 end

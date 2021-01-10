2e2f4832ecdcc6d6a35388e7e3e1eaaef3203360
Alertzero <faceroxz@gmail.com>
2021-01-10 22:11:33 +0700

    Add account activation18

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@sandboxfa20a7ada8074e03ae87babeb4c62059.com'
+  default from: 'postmaster@sandboxfa20a7ada8074e03ae87babeb4c62059.mailgun.org'
   layout 'mailer'
 end
